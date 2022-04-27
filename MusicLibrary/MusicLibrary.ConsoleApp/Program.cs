1f4c25ffeccad09a65e09b6243b954338b070cc3
Calin Andrei <andrei.calin@itperspectives.ro>
2022-04-27 16:06:08 +0300

    Base programm

---
+++ MusicLibrary/MusicLibrary.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MusicLibrary.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to Gold Library!");
+            var comanda =  Console.ReadLine();
+
+            // se verifica comanda ...
+
+        }
+    }
+}
