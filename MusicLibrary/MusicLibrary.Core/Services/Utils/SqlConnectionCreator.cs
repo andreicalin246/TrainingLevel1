2cf9d65d9c046e4ba5d2d163e9e726b033d522a4
Bogdan4120 <bogdan.basca@itperspectives.ro>
2022-07-11 11:27:04 +0300

    commit

---
+++ MusicLibrary/MusicLibrary.Core/Services/Utils/SqlConnectionCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MusicLibrary.Core.Services.Utils
+{
+	public sealed class SqlConnectionCreator
+	{
+		private const string SongLibraryConnectionString = "Server=(LocalDb)\\LocalDBMusicLibrary;  Database=MusicLibrary; Trusted_Connection=true";
+		private static readonly Lazy<SqlConnection> instance = new Lazy<SqlConnection>(new SqlConnection(SongLibraryConnectionString));
+
+		private SqlConnectionCreator()
+		{
+		}
+
+		public static SqlConnection Instance()
+		{
+			var obj = instance.Value;
+			if (obj.State != System.Data.ConnectionState.Open)
+			{
+				obj.Open();
+			}
+
+			return obj;
+		}
+	}
+}
